9b2c6431b7159e01975942e3a384f1d83356f780
prosperemebo <emeboprosper@gmail.com>
2024-07-04 01:11:20 +0100

    chore: initialised frontend

--- backend/controllers/user_controller.py
+++ backend/controllers/user_controller.py
@@ -4,6 +4,7 @@ from passlib.hash import pbkdf2_sha256
 
 from db import db
 from models import UserModel
+from schemas.user_schemas import UserSchema
 
 
 blp = Blueprint("Users", "users", description="Operations on users")
